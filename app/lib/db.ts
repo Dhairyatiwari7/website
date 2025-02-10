5ffd03a8a3c5dccd903887eb7533b1cecc4e8d9d
Dhairya Tiwari <dhairyatiwari186@gmail.com>
2025-02-10 16:26:14 +0530

    changes backend

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,11 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri ="mongodb+srv://quickcare:quickcare@cluster0.qpo69.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
-
-if (!uri) {
-  throw new Error("❌ MongoDB URI is missing!");
-}
-
+const uri = 'mongodb+srv://quickcare:quickcare@cluster0.qpo69.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -14,26 +9,9 @@ declare global {
 }
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, {
-    serverSelectionTimeoutMS: 30000,
-    socketTimeoutMS: 45000,
-  });
-
+  client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
-
 clientPromise = global._mongoClientPromise;
-
-export const connectToDB = async () => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("test"); // Ensure this is the correct DB name
-    console.log("✅ Connected to MongoDB:", db.databaseName);
-    return db;
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error);
-    throw new Error("Database connection failed");
-  }
-};
 
 export default clientPromise;
