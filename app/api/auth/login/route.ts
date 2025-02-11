43aa62ab90f5be321b6babe4dfe496d894e9f69a
Dhairya Tiwari <dhairyatiwari186@gmail.com>
2025-02-11 13:42:19 +0530

    eebf44e

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -9,12 +9,10 @@ export async function POST(req: Request) {
   try {
     await connectToDB();
 
-    const { username, password } = await req.json();
-
-    const role = req.headers.get("role");
+    const { username, password} = await req.json();
 
     if (!username || !password) {
-      return NextResponse.json({ message: "Username, password are required" }, { status: 400 });
+      return NextResponse.json({ message: "Username, password, and role are required" }, { status: 400 });
     }
 
     let user;
